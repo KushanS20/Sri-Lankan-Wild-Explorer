fbdd27687e501977c9de524f759bcb9b9ab2fc17
Codersauruse <shehandesk123@gmail.com>
2024-06-10 18:07:48 +0530

    added game controls and setup

--- Quiz/src/main/java/org/quiz/quizApplication.java
+++ Quiz/src/main/java/org/quiz/quizApplication.java
@@ -11,7 +11,7 @@ public class quizApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(quizApplication.class.getResource("quizView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 1000);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
